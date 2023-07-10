c77e387035700725d3340b5d185daf45887c83bf
alexhallam <alexhallam6.28@gmail.com>
2023-07-10 00:04:11 -0400

    working params

--- tablespoon/forecasters.py
+++ tablespoon/forecasters.py
@@ -152,6 +152,87 @@ class Naive(object):
         df_pred = pd.DataFrame(np_predictions, columns=["y_sim"])
         df_result = pd.concat([df_cross, df_pred], axis=1)
         return df_result
+    
+    def fitted_params(
+        self,
+        df_historical,
+        horizon=30,
+        frequency=None,
+        lag=1,
+        uncertainty_samples=5000,
+        include_history=False,
+    ):
+        """Fitted Parameters - forecast method
+
+        Args:
+            df_historical (pd.DataFrame): A date sorted dataframe with the columns `ds` and `y`
+            horizon (int, optional): Forecast horizon. Defaults to 30.
+            frequency (int, optional): number of rows that make a seasonal period. Defaults to None.
+            lag (int, optional): number of rows that make a seasonal period. Defaults to 1.
+            uncertainty_samples (int, optional): number of uncertainty samples to draw. Defaults to 5000.
+            include_history (bool, optional): include history. Defaults to False.
+            chain_ids (str, optional): identifiers for chain ids. Defaults to None.
+            verbose (bool, optional): verbose. Defaults to False.
+
+        Returns:
+            pd.DataFrame: A dataframe of predictions as `y_sim`
+
+        Example:
+            ```py
+            import pandas as pd
+            import tablespoon as tbsp
+            from tablespoon.data import APPL
+            df_APPLE = APPL
+            df_APPLE = df_APPLE.assign(ds = lambda df: pd.to_datetime(df.ds))
+            naive = tbsp.Naive()
+            df_f = (naive.predict(df_APPLE, horizon=7*4, frequency="D", lag = 1, uncertainty_samples = 500).assign(model = 'naive'))
+            df_f.head(10)
+            ```
+        """
+        if frequency is None:
+            send_helpful_frequency_error()
+        self.y = df_historical["y"]
+        self.history_dates = get_sorted_dates(df_historical)
+        last_date = self.history_dates.max()
+        min_date = self.history_dates.min()
+        check_historical_dates_are_contiguous(
+            self.history_dates, min_date, last_date, frequency
+        )
+        dates = pd.date_range(
+            start=last_date, periods=horizon + 1, freq=frequency
+        )  # An extra in case we include start  # 'M','D', etc.
+        dates = dates[dates > last_date]  # Drop start if equals last_date
+        dates = dates[:horizon]  # Return correct number of periods
+        if include_history:
+            dates = np.concatenate((np.array(self.history_dates), dates))
+        df_dates = pd.DataFrame({"ds": dates})
+        df_samples = pd.DataFrame({"rep": np.arange(uncertainty_samples)})
+        df_cross = df_dates.merge(df_samples, how="cross")
+        # fit
+        t = lag + 1
+        t_lag = t - lag
+        y = self.y.to_numpy()
+        end = len(y) - lag
+        yt = y[t:]
+        yt_lag = y[t_lag:end]
+        y_last = y.take(-1)
+        mod = sm.GLM(yt, yt_lag, family=sm.families.Gaussian())
+        sigma = np.sqrt(mod.fit().scale)
+        rng = np.random.default_rng()
+        forecast = np.empty([uncertainty_samples, horizon])
+        # fitted params
+        params_list = []
+        for h in range(0, horizon):
+            params_dict = {}
+            # make dictionary of parameters
+            params_dict["ds"] = dates[h]
+            params_dict["mu"] = np.round(y_last, 2)
+            params_dict["sigma"] = np.round(sigma * np.sqrt(h + 1),2)
+            mu = params_dict["mu"]; sd = params_dict["sigma"];
+            params_dict['dist'] = f"N({mu}, {sd})"
+            params_list.append(params_dict)
+        return pd.DataFrame(params_list)
+
 
 
 class Mean(object):
@@ -237,6 +318,87 @@ class Mean(object):
         df_pred = pd.DataFrame(np_predictions, columns=["y_sim"])
         df_result = pd.concat([df_cross, df_pred], axis=1)
         return df_result
+    
+    def fitted_params(
+        self,
+        df_historical,
+        horizon=30,
+        frequency=None,
+        uncertainty_samples=5000,
+        include_history=False,
+    ):
+        """Fitted Parameters
+
+        Args:
+            df_historical (pd.DataFrame): A date sorted dataframe with the columns `ds` and `y`
+            horizon (int, optional): Forecast horizon. Defaults to 30.
+            frequency (int, optional): number of rows that make a seasonal period. Defaults to None.
+            lag (int, optional): number of rows that make a seasonal period. Defaults to 1.
+            uncertainty_samples (int, optional): number of uncertainty samples to draw. Defaults to 5000.
+            include_history (bool, optional): include history. Defaults to False.
+            chain_ids (str, optional): identifiers for chain ids. Defaults to None.
+            verbose (bool, optional): verbose. Defaults to False.
+
+        Returns:
+            pd.DataFrame: A dataframe of predictions as `y_sim`
+
+        Example:
+            ```py
+            import pandas as pd
+            import tablespoon as tbsp
+            from tablespoon.data import APPL
+            df_APPLE = APPL
+            df_APPLE = df_APPLE.assign(ds = lambda df: pd.to_datetime(df.ds))
+            mean = tbsp.Mean()
+            df_f = (n.predict(df_APPLE, horizon=7*4, frequency="D", lag = 1, uncertainty_samples = 500).assign(model = 'mean'))
+            df_f.head(10)
+            ```
+        """
+        self.y = df_historical["y"]
+        self.history_dates = get_sorted_dates(df_historical)
+        last_date = self.history_dates.max()
+        min_date = self.history_dates.min()
+        check_historical_dates_are_contiguous(
+            self.history_dates, min_date, last_date, frequency
+        )
+        dates = pd.date_range(
+            start=last_date, periods=horizon + 1, freq=frequency
+        )  # An extra in case we include start  # 'M','D', etc.
+        dates = dates[dates > last_date]  # Drop start if equals last_date
+        dates = dates[:horizon]  # Return correct number of periods
+        if include_history:
+            dates = np.concatenate((np.array(self.history_dates), dates))
+        df_dates = pd.DataFrame({"ds": dates})
+        df_samples = pd.DataFrame({"rep": np.arange(uncertainty_samples)})
+        df_cross = df_dates.merge(df_samples, how="cross")
+        # fit
+        y = self.y
+        T = len(y)
+        deg_freedom = T - 1
+        mu, sigma = norm.fit(y)
+        rng = np.random.default_rng()
+        forecast = np.empty([uncertainty_samples, horizon])
+        for h in range(0, horizon):
+            forecast[:, h] = mu + sigma * np.sqrt(1 + (1 / T)) * rng.standard_t(
+                df=deg_freedom, size=uncertainty_samples
+            )
+        np_predictions = forecast.transpose().reshape(
+            uncertainty_samples * horizon, 1
+        )
+        df_pred = pd.DataFrame(np_predictions, columns=["y_sim"])
+        df_result = pd.concat([df_cross, df_pred], axis=1)
+        ## fitted params
+        params_list = []
+        for h in range(0, horizon):
+            params_dict = {}
+            # make dictionary of parameters
+            params_dict["ds"] = dates[h]
+            params_dict["mu"] = np.round(mu, 2)
+            params_dict["sigma"] = np.round(sigma * np.sqrt(1 + (1 / T)),2)
+            mu = params_dict["mu"]; sd = params_dict["sigma"];
+            params_dict['dist'] = f"N({mu}, {sd})"
+            params_list.append(params_dict)
+        return pd.DataFrame(params_list)
 
 
 class Snaive(object):
@@ -380,9 +542,8 @@ class Snaive(object):
         yt = y[lag:last_end]
         yt_lag = y[0:last_start]
         mod = sm.GLM(yt, yt_lag, family=sm.families.Gaussian())
-        sigma = np.sqrt(mod.fit().scale) # attempt to use glm to get sigma
-        rng = np.random.default_rng()
-        #params = np.empty([uncertainty_samples, horizon])
+        # the variable sigma represents the estimated standard deviation of the residuals from the generalized linear model (GLM) fit.
+        sigma = np.sqrt(mod.fit().scale)
         params_list = []
         for h in range(0, horizon):
             params_dict = {}
@@ -390,14 +551,7 @@ class Snaive(object):
             params_dict["ds"] = dates[h]
             params_dict["mu"] = y[(len(y)) - (lag - ((h) % lag))]
             params_dict["sigma"] = np.round(sigma * np.sqrt(np.trunc(((h) * 1) / (lag)) + 1),2)
-            params_dict["var"] = np.round((sigma * np.sqrt(np.trunc(((h) * 1) / (lag)) + 1))**2,2)
-            mu = params_dict["mu"]; var = params_dict["var"];
-            params_dict['dist'] = f"N({mu}, {var})"
+            mu = params_dict["mu"]; sd = params_dict["sigma"];
+            params_dict['dist'] = f"N({mu}, {sd})"
             params_list.append(params_dict)
         return pd.DataFrame(params_list)
-
-# make a dummy class that prints hello world
-class HelloWorld(object):
-    def hello_world(self):
-        print("Hello World!")
-        return None
